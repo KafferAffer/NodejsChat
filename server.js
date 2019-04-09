73dc3d55fb1aab57e6288d001d27ae874fe1fca6
KafferAffer <46445011+KafferAffer@users.noreply.github.com>
2019-04-09 15:52:47 +0200

    Login side

--- server.js
+++ server.js
@@ -3,31 +3,34 @@ var express = require('express');
 var http = require('http');
 var fs = require('fs');
 var app = express();
+var socket = require('socket.io');
 var port = process.env.PORT || 3000;
 var session = require('express-session');
 var admincdb = require('./Createdb');
+var bodyParser = require('body-parser');
 
-app.use(session({secret: 'CCS'}));
+server = app.listen(port);
 
-var server = http.createServer(function(req,res){
-    switch(req.url){
-        case '/':
-            fs.createReadStream(__dirname + '/views/login.html').pipe(res);
-            break;
-        case '/admincdb':
-            fs.createReadStream(__dirname + '/views/admincdb.html').pipe(res);
-            admincdb.createall();
-            break;
-        
-    }
+//app.use(express.static('views'));
+app.use(bodyParser());
+app.use(session({secret: 'thisisarandomstring'}));
+
+app.get('/',function(req,res){
+    res.sendfile('views/index.html');
 });
 
-var io = require('socket.io')(server);
+app.get('/logged',function(req,res){
+    res.write('youre logged in');
+});
+
+app.post('/test/login',function(req,res){
+    var username = req.body.username
+    console.log("username = " + username); 
+    //res.redirect('/logged');
+});
+
+var io = socket(server);
 
 io.on('connection', function(socket){
     console.log('a user connected');
 });
-
-server.listen(port,function(){
-    console.log("Socket Server is running")
-})
